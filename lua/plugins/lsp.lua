62aeaf67120713bb571b886642038a4a70a49cb1
Amirzan Jeyakumar <jeyakumaramirzan@gmail.com>
2024-11-22 00:12:31 +0000

    remove stuff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,1 @@
-return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "mason.nvim",
-      "telescope.nvim",
-      "which-key.nvim",
-    },
-    opts = function(_, opts)
-      opts.inlay_hints = { enabled = false }
-      return opts
-    end,
-  },
-}
+return {}
